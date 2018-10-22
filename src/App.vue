b75cbfe4467b466e4c0c5f13c48306dbded3d48e
Joe Weston <joeweston6@gmail.com>
2018-10-22 12:33:28 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <p> {{ msg }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
